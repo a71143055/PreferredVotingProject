fa2e3add595ee58fecf22f5f45a64ad647b0c460
a71143055 <a71143055@gmail.com>
2025-06-30 14:44:45 +0900

    PV

--- app/src/main/java/kr/ac/kopo/preferencevoting/ResultActivity.java
+++ app/src/main/java/kr/ac/kopo/preferencevoting/ResultActivity.java
@@ -1,6 +1,11 @@
 package kr.ac.kopo.preferencevoting;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,27 @@ public class ResultActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Intent intent = getIntent();
+        int[] voteCount = intent.getIntArrayExtra("voteCount");
+        String[] imageName = intent.getStringArrayExtra("imgName");
+
+        TextView tv[] = new TextView[imageName.length];
+        RatingBar rbar[] = new RatingBar[imageName.length];
+
+        Integer tvID[] = {R.id.tv0,R.id.tv1,R.id.tv2,R.id.tv3,R.id.tv4,R.id.tv5,R.id.tv6,R.id.tv7,R.id.tv8};
+        Integer rbarID[] = {R.id.rating0,R.id.rating1,R.id.rating2,R.id.rating3,R.id.rating4,R.id.rating5,R.id.rating6,R.id.rating7,R.id.rating8};
+
+        for (int i = 0; i < voteCount.length; i++) {
+            tv[i].setText(imageName[i]);
+            rbar[i].setRating((float) voteCount[i]);
+        }
+        Button btnReturn = (Button) findViewById(R.id.btn_back);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
